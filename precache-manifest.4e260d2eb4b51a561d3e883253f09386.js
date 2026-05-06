fc57aa8b4c4b7b5550ccd78ee0860862104ad9fa
Awu Chen <awwu@Awus-MacBook-Pro.local>
2026-05-06 01:02:01 -0500

    Updates

---
+++ precache-manifest.4e260d2eb4b51a561d3e883253f09386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324d332d1931955db384ba2b9f4be3fd",
+    "url": "/iFlyNet/index.html"
+  },
+  {
+    "revision": "8aca87e64ec5b8fc5a31",
+    "url": "/iFlyNet/static/css/main.ead59979.chunk.css"
+  },
+  {
+    "revision": "2a483efceadf0847eb48",
+    "url": "/iFlyNet/static/js/2.e9d286b3.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/iFlyNet/static/js/2.e9d286b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aca87e64ec5b8fc5a31",
+    "url": "/iFlyNet/static/js/main.29785797.chunk.js"
+  },
+  {
+    "revision": "825646aa0421bddc6d31",
+    "url": "/iFlyNet/static/js/runtime-main.933835e8.js"
+  }
+]);
